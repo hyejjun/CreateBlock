9d3b1c485c68f6bcf020ba4c0a71ec1dfe32090d
hyejjun <hjlee9725@gmail.com>
2021-09-02 12:33:35 +0900

    Block 검증

--- src/block0902.js
+++ src/block0902.js
@@ -21,7 +21,7 @@ class BlockHeader {
 class Block {
     constructor(header, body) {
         this.header = header,       //BlockHeader 에서 만든값을 넣겠다. 객체 안에 객체가 들어가게 된다.
-            this.body = body
+        this.body = body
     }
 }
 
@@ -40,25 +40,25 @@ function getLastBlock() {
 
 
 function createGenesisBlock() {
-    const version = getVersion()    
-    const index = 0                 
-    const time = getCurrentTime()  
-    const previousHash = '0'.repeat(64)   
+    const version = getVersion()
+    const index = 0
+    const time = getCurrentTime()
+    const previousHash = '0'.repeat(64)
 
     const body = ['hello block']
-    const tree = merkle('sha256').sync(body)             
-    const root = tree.root() || '0'.repeat(64)            
+    const tree = merkle('sha256').sync(body)
+    const root = tree.root() || '0'.repeat(64)
 
     const header = new BlockHeader(version, index, previousHash, time, root)
     return new Block(header, body)
 }
 
 // 다음 블럭의 header 와 body를 만들어주는 함수
-function nextBlock(data){
+function nextBlock(data) {
     // header
     const prevBlock = getLastBlock()    // 맨 마지막 블럭의 정보를 가져옴
     const version = getVersion()
-    const index = prevBlock.header.index+1
+    const index = prevBlock.header.index + 1
     const previousHash = createHash(prevBlock)
     const time = getCurrentTime()
 
@@ -69,14 +69,14 @@ function nextBlock(data){
     return new Block(header, data)
 }
 
-function createHash(block){
+function createHash(block) {
     const {
         version,
         index,
         previousHash,
         time,
         merkleRoot
-    } = block.header 
+    } = block.header
     const blockString = version + index + previousHash + time + merkleRoot
     const Hash = CryptoJs.SHA256(blockString).toString()
     return Hash
@@ -86,11 +86,11 @@ function createHash(block){
 // 다음 블럭을 생성할 형태는 다른 곳에 만들거임
 function addBlock(data) {
     const newBlock = nextBlock(data)
-    if(isVaildNewBlock(newBlock, getLastBlock())){
+    if (isVaildNewBlock(newBlock, getLastBlock())) {
         Blocks.push(newBlock)
         return true;    // 함수를 여기서 끝나게 하고
     } return false;     // 함수를 종료시켜라
-    
+
 }
 
 addBlock(['hello1'])
@@ -99,19 +99,49 @@ addBlock(['hello3'])
 
 /* etc */
 
-function isVaildNewBlock(currentBlock, previousBlock){
-    isVaildType(currentBlock)
+function isVaildNewBlock(currentBlock, previousBlock) {
+    // currentBlock 에 대한 header, body, DataType 을 확인
+    if (!isVaildType(currentBlock)) {
+        console.log(`invaild block structure ${JSON.stringify(currentBlock)}`);
+        return false    // 이 조건이 걸리면 함수 종료 - 그래서 else if 안쓰고 if 문으로 쭉 쭉 써도 된다.
+    }
+
+    // index 값이 유효한지 체크한다. - 만들 블럭에서 -1을 하면 이전 블럭 index 와 같아야함
+    // 이전 블럭에서 +1 을 하면 만들 블럭 index 랑 같아야 함
+    if (previousBlock.header.index + 1 !== currentBlock.header.index) {
+        console.log(`invaild index`);
+        return false
+    }
+
+    // previousHash 체크
+    if (createHash(previousBlock) !== currentBlock.header.previousHash) {
+        console.log(`invalid previousBlock`);
+        return false
+    }
+
+    // Body Check - body 가 비어있는 경우
+    if (currentBlock.body.length === 0) {
+        console.log(`invalid body`);
+        return false
+    }
+    // merkel root 가 같지 않은 경우
+    if (merkle("sha256").sync(currentBlock.body).root() !== currentBlock.header.merkleRoot) {
+        console.log(`invalid merkelRoot`);
+        return false
+    }
+
     return true
 }
 
-function isVaildType(block){
-    console.log(typeof(block.header.version))           // string
-    console.log(typeof(block.header.index))             // number
-    console.log(typeof(block.header.previousHash))      // string
-    console.log(typeof(block.header.time))              // number
-    console.log(typeof(block.header.merkleRoot))        // string
-    console.log('-----------------------');
-
+function isVaildType(block) {
+    return (
+        typeof (block.header.version) === "string" &&           // string
+        typeof (block.header.index) === "number" &&            // number
+        typeof (block.header.previousHash) === "string" &&     // string
+        typeof (block.header.time) === "number" &&             // number
+        typeof (block.header.merkleRoot) === "string" &&       // string
+        typeof (block.body) === "object"                    // object
+    )
 }
 
 
@@ -124,5 +154,36 @@ function getCurrentTime() {
     return Math.ceil(new Date().getTime() / 1000)
 }
 
-//console.log(Blocks);
 
+
+
+/* 전체 배열 검증 */
+function isVaildBlock(Blocks) {
+    // 제네시스 블럭이 유효한지?
+    if (JSON.stringify(Blocks[0]) !== JSON.stringify(createGenesisBlock())) {
+        console.log(`GenensisBlock error`)
+        return false
+    }
+
+    // Blocks 총 3개 있는데
+    // 1부터 3까지 반복 - 총 두번이 도는 for 문 - 첫번째는 검사하지 않기 위해 i=1 부터
+    let tempBlocks = [Blocks[0]]
+    for (let i = 1; i < Blocks.length; i++) {
+        if (isVaildNewBlock(Blocks[i], tempBlocks[i - 1])) {
+            tempBlocks.push(Blocks[i])
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+// console.log(Blocks);
+
+
+module.exports ={
+    getBlocks,
+    getLastBlock,
+    addBlock,
+    getVersion,
+}
