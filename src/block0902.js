78ef2e395ae6684ebf2b9a933e85ce0920faed6e
hyejjun <hjlee9725@gmail.com>
2021-09-08 14:10:30 +0900

    마이닝-difficulty and nonce

--- src/block0902.js
+++ src/block0902.js
@@ -3,19 +3,26 @@ const merkle = require('merkle')
 const CryptoJs = require('crypto-js')
 const SHA256 = require('crypto-js/sha256')
 const random = require('random')
-
+const { hexToBinary } = require('./utils')
 
 // const tree = merkle("sha256").sync(['aaaa'])
 // tree.root() 
 
+// 블럭의 개수의 따라서 난이도 조정
+const BLOCK_GENERATION_INTERVAL = 10    //10초 기준
+const BLOCK_ADJUSTIMENT_INTERVAL = 10   // 블럭이 10개가 넘을 때마다 난이도 변경
+
 // 헤더 만들기
 class BlockHeader {
-    constructor(version, index, previousHash, time, merkleRoot) {
+    constructor(version, index, previousHash, time, merkleRoot, difficulty, nonce) {
         this.version = version      //  1 new 생성자를 통해서 this.version 은 {version:1} 이렇게 된다
         this.index = index          // 2 {version:1, index : 2}
         this.previousHash = previousHash
         this.time = time
         this.merkleRoot = merkleRoot
+        // 0908
+        this.difficulty = difficulty    // 문제 난이도
+        this.nonce = nonce              // 문제 몇번 시도 했는지
     }
 }
 
@@ -50,7 +57,10 @@ function createGenesisBlock() {
     const tree = merkle('sha256').sync(body)
     const root = tree.root() || '0'.repeat(64)
 
-    const header = new BlockHeader(version, index, previousHash, time, root)
+    const difficulty = 0
+    const nonce = 0
+
+    const header = new BlockHeader(version, index, previousHash, time, root, difficulty, nonce)
     return new Block(header, body)
 }
 
@@ -62,13 +72,73 @@ function nextBlock(data) {
     const index = prevBlock.header.index + 1
     const previousHash = createHash(prevBlock)
     const time = getCurrentTime()
+    const difficulty = getDifficulty(getBlocks())    //<- 함수를 만들어야함..
 
     const merkleTree = merkle("sha256").sync(data)
     const merkleRoot = merkleTree.root() || '0'.repeat(64)
 
-    const header = new BlockHeader(version, index, previousHash, time, merkleRoot)
+    const header = findBlock(version, index, previousHash, time, merkleRoot, difficulty)
     return new Block(header, data)
 }
+
+function getDifficulty(blocks) {
+    // 시간에 관련되서 처리
+    // 마지막 블럭가져오기
+    const lastBlock = blocks[blocks.length - 1]
+
+    if (lastBlock.header.index % BLOCK_ADJUSTIMENT_INTERVAL === 0 && lastBlock.header.index !== 0) {
+        // 10으로 나눈 값의 나머지가 0 이 되었을 때 -> 난이도 조정 && 제네시스 블럭이 아니어야함
+        return getAdjustedDifficulty(lastBlock, blocks)  // 마지막 배열과 내가 전체 갖고있는 블럭을 인자값으로 던져야함
+    }
+    return lastBlock.header.difficulty      // 조건 해당없으면 이전블럭의 난이도와 동일
+}
+
+function getAdjustedDifficulty(lastBlock, blocks) {
+    const prevAdjustmentBlock = blocks[blocks.length - BLOCK_ADJUSTIMENT_INTERVAL]      // 총 배열 20개 -10 하면 10번째 애를 가져오게됨
+    const timeToken = lastBlock.header.time - prevAdjustmentBlock.header.time           // 그러면 시간이 얼마만큼 차이가 나는지 알 수 있다.
+    const timeExpected = BLOCK_ADJUSTIMENT_INTERVAL * BLOCK_GENERATION_INTERVAL         // 
+
+    // 예상시간보다 빨리만들었으면 난이도 올림
+    if (timeToken < timeExpected / 2) {
+        return prevAdjustmentBlock.header.difficulty + 1
+    } else if (timeToken > timeExpected * 2) {
+        // 예상 시간보다 너무 느린데 싶으면 난이도 낮춤
+        return prevAdjustmentBlock.header.difficulty - 1
+    } else {
+        // 예상시간대로 잘 가고 있으면 그대로
+        return prevAdjustmentBlock.header.difficulty
+    }
+}
+
+function findBlock(version, index, previousHash, time, merkleRoot, difficulty) {
+    let nonce = 0
+    // 여기서 검증처리 해야함 - 무한 반복 돌거임 - 조건이 맞아야만 빠져나올 수 있도록
+    while (true) {
+        // 이 상태에서는 block 이 없다.
+        // 여기서 createHeaderHash 함수 호출할거임 그래서 인자값으로 nonce 값 줄 수 있음
+        let hash = createHeaderHash(version, index, previousHash, time, merkleRoot, difficulty, nonce)
+        console.log(hash);
+        console.log('-----------');
+        if (hashMatchDifficulty(hash, difficulty)) {       // 우리가 앞으로 만들 header의 hash 값의 앞자리 0이 몇개인가?
+            return new BlockHeader(version, index, previousHash, time, merkleRoot, difficulty, nonce)
+        }
+        nonce++
+    }
+}
+
+// 두 가지 인자값을 받음 hash 와 difficulty(= 0이 몇개인지에 따라 난이도가 달라짐)
+function hashMatchDifficulty(hash, difficulty) {
+    // hash 16진수 -> 2진수
+    const hashBinary = hexToBinary(hash)    // 아까 만든 함수에 hash 를 넣어서 16->2 진수로 변경함
+    const prefix = '0'.repeat(difficulty)   // difficulty 갯수만큼 0 반복    
+    return hashBinary.startsWith(prefix)    // 얘의 결과 는 boolean 형태
+}
+
+function createHeaderHash(version, index, previousHash, time, merkleRoot, difficulty, nonce) {
+    let txt = version + index + previousHash + time + merkleRoot + difficulty + nonce
+    return CryptoJs.SHA256(txt).toString().toUpperCase()
+}
+
 
 function createHash(block) {
     const {
@@ -94,17 +164,17 @@ function addBlock(newBlock) {
 
 }
 
-function mineBlock(blockData){
+function mineBlock(blockData) {
     const newBlock = nextBlock(blockData)        //새로운 객체를 가진, Object Block {header, body}
     // 블럭을 만들려면 전역배열에다 넣어줘야함. 검증은 addBlock 이 대신해줄것
     // addBlock 도 매개변수 받는걸로 nextBlock을 쓴다.
     // 그래서 addBlock 은 newBlock을 받아서 하는거로 바꿔준다
-    if(addBlock(newBlock)){
+    if (addBlock(newBlock)) {
         // 모든 peer 에게 블록이 추가 되었으니 마지막거를 확인하라고 broadcast로 날려줘야 한다
         const nw = require('./network')
         nw.broadcast(nw.responseLastMsg())
         return newBlock
-    }else{
+    } else {
         return null
     }
 }
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+function hexToBinary(s){
+    const lookup ={
+        "0" : "0000", "1" : "0001", "2" : "0010", "3" : "0011",
+        "4" : "0100", "5" : "0101", "6" : "0110", "7" : "0111",
+        "8" : "1000", "9" : "1001", "A" : "1010", "B" : "1011",
+        "C" : "1100", "D" : "1101", "E" : "1110", "F" : "1111",
+    }
+
+    let rst =""     
+    for(let i=0; i<s.length; i++){
+        if(lookup[s[i]] === undefined) return null
+        rst += lookup[s[i]]
+    }
+
+    return rst
+    
+}
+
+module.exports = {
+    hexToBinary
+}
